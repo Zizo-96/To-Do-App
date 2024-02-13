78cdde4290c0e04abdf17e4a9741619d0ea38c64
Zizo-96 <abdelaziz.ahmed1996@gmail.com>
2024-02-14 01:54:37 +0200

    Created the project

---
+++ to-do-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='main-container'>
+      <h1 id="first-message"> Create your list and hope for the best </h1>
+      <div>
+        <input type="text" id='input-field'/>
+        <button> Add Task </button>
+        </div>
+    </div>
+  );
+}
+
+export default App;

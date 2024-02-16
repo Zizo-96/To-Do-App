321534822ab47be811f39079fef47496c84fd2b4
Zizo-96 <abdelaziz.ahmed1996@gmail.com>
2024-02-16 16:02:02 +0200

    Refined the styling

--- to-do-app/src/ItemList.js
+++ to-do-app/src/ItemList.js
@@ -6,7 +6,7 @@ function ItemList() {
     const [allTasks, setAllTasks] = useState([]);
 
     const handleChange= (e)=>{
-        setTask(e.target.value);
+        setTask(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1));
     }
 
    const handleClick = ()=>{
@@ -27,14 +27,14 @@ function ItemList() {
 
   return (
     <div>
-        <input value={task} onChange={handleChange} type="text" id='input-field' onKeyDown={(e)=> (e.key ==="Enter") && handleClick()}/>
+        <input placeholder="What's on your agenda?" value={task} onChange={handleChange} type="text" id='input-field' onKeyDown={(e)=> (e.key ==="Enter") && handleClick()}/>
         <button id='btn-add' onClick={handleClick}> Add Task </button>
         {allTasks.map((toDo)=>{
             return (
             <div className='list-section'>
                 <input type="checkbox" name="" id="checkBox" />
                 <p>{toDo}</p>
-                <button id='btn-delete'>x</button>
+                <button id='btn-delete'>Delete</button>
             </div>
            
         )})}
